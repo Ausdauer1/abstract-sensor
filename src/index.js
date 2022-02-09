ebc64dce6b910d668a58e5dfbc1a45dc41e05055
Ausdauer1 <steady051@naver.com>
2022-02-09 17:36:57 +0900

    매운맛 완료

--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
-class Sensor {}
+class Sensor {
+    constructor(deviceId) {
+        this.powerStatus = 'off';
+        this.reportingInterval = 10000;
+        this.deviceId = deviceId;
+    }
+    turn(Switch) {
+        if (this.powerStatus === 'off') {
+            this.powerStatus = Switch;
+            this.status = 'idle';
 
-class IotServer {}
+            setTimeout(() => {
+                this.status = 'sensingDistance';
+            }, this.reportingInterval);
+
+            setTimeout(() => {
+                this.status = 'reportingData';
+            }, this.reportingInterval + 500);
+
+            setTimeout(() => {
+                this.status = 'idle';
+            }, this.reportingInterval + 1500);
+        } else if (this.powerStatus === 'on' && Switch === 'on') {
+            throw new Error('아이고야 틀렸뿟네 다시 해라잉');
+        } else {
+            this.powerStatus = Switch;
+        }
+    }
+}
+class IotServer {
+    constructor() {}
+
+    start(newSensor) {
+        this.sensor = newSensor;
+    }
+
+    publish(action) {
+        if (this.sensor[0].powerStatus !== 'off') {
+            if (action.actionId === 'CHANGE_REPORTING_INTERVAL') {
+                this.sensor[0].reportingInterval = action.payload;
+            }
+        }
+    }
+}
 
 module.exports = {
     Sensor,
